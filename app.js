ff385256f3b0968681d932bcb5c402b171e0db99
chebur112 <kyzlakovi@bk.ru>
2025-05-03 22:52:51 +0800

    Main page

---
+++ app.js
@@ -0,0 +1,25 @@
+const btnDoc = document.getElementById("doc");
+const btnRules = document.getElementById("rules");
+
+const modal1 = document.getElementById("text1");
+const modal2 = document.getElementById("text2");
+
+btnDoc.onclick = () => modal1.style.display = "block";
+btnRules.onclick = () => modal2.style.display = "block";
+
+
+document.querySelectorAll(".close").forEach(btn => {
+  btn.onclick = () => {
+    const targetId = btn.getAttribute("data-target");
+    document.getElementById(targetId).style.display = "none";
+  };
+});
+
+// Закрытие по клику вне окна
+window.onclick = (event) => {
+  [modal1, modal2].forEach(modal => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+};
